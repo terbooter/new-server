14890b7e48c4c990f01cfe53c85c6d9434ad56cd
dmitry <dmitry@klassapp.com>
2018-11-08 08:44:35 +0300

    generate ssh key

--- install-docker.sh
+++ install-docker.sh
@@ -24,3 +24,5 @@ sudo apt-get install -y docker-ce=18.03.1~ce-0~ubuntu
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
