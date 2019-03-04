4d8eb9d9e883ca68cfac1aaa46f6d8980e57b63e
dmitry <dmitry@klassapp.com>
2019-03-04 22:48:34 +0300

    update docker to 18.06.3~ce~3-0~ubuntu

--- install-docker.sh
+++ install-docker.sh
@@ -21,7 +21,7 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
-sudo apt-get install -y docker-ce=18.03.1~ce-0~ubuntu
+sudo apt-get install -y docker-ce=18.06.3~ce~3-0~ubuntu
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
