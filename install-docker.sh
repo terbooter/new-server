adfc7884d9e431b6cd503cfb430d22e51bc82f4c
dmitry <dmitry@klassapp.com>
2019-03-04 22:59:42 +0300

    update docker-compose to 1.22.0

--- install-docker.sh
+++ install-docker.sh
@@ -23,7 +23,7 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce=18.06.3~ce~3-0~ubuntu
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
